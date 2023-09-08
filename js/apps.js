3ba3aa4ac1f2faacd50031462259c3905b285b83
Cuello Ignacio <cuelloignacio0@gmail.com>
2023-09-08 16:53:07 -0300

    implementacion de urlApi y agregado de style

--- js/apps.js
+++ js/apps.js
@@ -1,10 +1,35 @@
 console.log("el archivo javascrip ya está importado");
 
-const libros = {
+const urlApi="https://biblioisp20-7912.restdb.io/rest/libros?apikey=64f8c71068885441e60bfe8a";
+
+//
+const appLibros = {
     listarLibros: () =>{
-        const contenedor= document.getElementById("contenedorLibros");
-        const books="<ul><li> libro1 <li> libro2 </ul>";
-        contenedor.innerHTML=books;
+        //tomamos la referencia al contenedor donde se mostraran los libros
+        const contenedor=document.getElementById("contenedorLibros");
+
+        //creamos una variable vacia 
+        let contenidoHTML= "";
+
+        //
+        fetch(urlApi).then(respuesta=>respuesta.json()).then(libros=>{
+            console.log(libros);
+            for(const libro of libros ) {
+                contenidoHTML += `
+                <div>
+                    <img src="${libro.portada_url} "class="img-thumbnail"/>
+                    <details>
+                    <summary> ${libro.nombre}</summary>
+                    ${libro.sinopsis}
+                    </details>
+
+                </div>`;
+
+            };
+        console.log(contenidoHTML);
+        contenedor.innerHTML=contenidoHTML;
+        })
+
     }
 }
-libros.listarLibros();
+appLibros.listarLibros();
